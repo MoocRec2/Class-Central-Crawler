a9c3daabc224319fa06fc54f2e05963f08e7ba06
Sameer Mohamed <smrbasil4@gmail.com>
2019-08-30 13:21:24 +0530

    Converted Script Thread into a Method and called from the main script

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from subject import retrieve_courses_from_subject
+from thread import retrieve_thread_of_course
+from db_connector import Course
+from pprint import pprint
 
 subjects_info_list = [
     {'key': 'ai', 'url': 'https://www.classcentral.com/subject/ai'},
     {'key': 'bl0ck_crypt0', 'url': 'https://www.classcentral.com/subject/blockchain-cryptocurrency'},
+    {'key': 'stats', 'url': 'https://www.classcentral.com/subject/statistics'},
 ]
-for subject_info in subjects_info_list:
-    retrieve_courses_from_subject(subject_info)
+# Retrieve Course of Subject
+# for subject_info in subjects_info_list:
+#     retrieve_courses_from_subject(subject_info)
+#
+# courses = list(Course.get_courses({'platform': 'Coursera'}))
+
+# Retrieve Threads of Course
+# for course in courses:
+#     retrieve_thread_of_course(course)
+#     break
+
+retrieve_thread_of_course({'course_link': 'https://www.classcentral.com/course/coursera-machine-learning-835'})
--- thread.py
+++ thread.py
@@ -14,82 +14,87 @@ from db_connector import CourseraThreads
 from pprint import pprint
 from selenium.webdriver.chrome.options import Options
 
-start_time = time.time()
 
-base_url = 'https://www.classcentral.com'
+def retrieve_thread_of_course(course):
+    start_time = time.time()
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-driver = webdriver.Chrome('C:/chromedriver', options=options)
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome('C:/chromedriver', options=options)
 
-course_url = 'https://www.classcentral.com/course/coursera-machine-learning-835'
+    course_url = course['course_link']
+    # course_url = 'https://www.classcentral.com/course/coursera-machine-learning-835'
 
-course = Course.get_course({'course_link': course_url})
+    course = Course.get_course({'course_link': course_url})
 
-driver.get(course_url)
+    driver.get(course_url)
 
-# ----- Course Description -----
-article_elem = driver.find_element_by_tag_name('article')
-div_elem = article_elem.find_element_by_tag_name('div')
-description = div_elem.text
-course['description'] = description
+    # ----- Course Description -----
+    article_elem = driver.find_element_by_tag_name('article')
+    div_elem = article_elem.find_element_by_tag_name('div')
+    description = div_elem.text
+    course['description'] = description
 
-db_operation_status = Course.upsert_courses_alt([course])
-if not db_operation_status:
-    print('Additional course information could not be saved to the database')
+    db_operation_status = Course.upsert_courses_alt([course])
+    if not db_operation_status:
+        print('Additional course information could not be saved to the database')
 
-# ----- Reviews (Posts of Thread) -----
-review_div_elem = driver.find_element_by_id('reviews-items')
-review_items_elems = review_div_elem.find_elements_by_xpath('./div')
-print('No. of Review Items:', review_items_elems.__len__())
+    # ----- Reviews (Posts of Thread) -----
+    review_div_elem = driver.find_element_by_id('reviews-items')
+    review_items_elems = review_div_elem.find_elements_by_xpath('./div')
+    print('No. of Review Items:', review_items_elems.__len__())
 
-reviews = []
+    reviews = []
 
-# TODO: Pagination
+    # TODO: Pagination
 
-for review_item_elem in review_items_elems:
-    review = {}
+    for review_item_elem in review_items_elems:
+        review = {}
 
-    # Title
+        # Title
 
-    # Content
-    read_more = 0
-    try:
-        read_more_btn_elem = review_item_elem.find_element_by_xpath(
-            './div[@class=\'row\']/div/div[@class=\'review-content text-2 margin-vert-small\']/div/button')
-        read_more_btn_elem.click()
-        read_more += 1
-    except NoSuchElementException:
-        pass
-    review_content_elem = review_item_elem.find_element_by_xpath(
-        './div[@class=\'row\']/div/div[@class=\'review-content text-2 margin-vert-small\']')
-    content = review_content_elem.text
+        # Content
+        read_more = 0
+        try:
+            read_more_btn_elem = review_item_elem.find_element_by_xpath(
+                './div[@class=\'row\']/div/div[@class=\'review-content text-2 margin-vert-small\']/div/button')
+            read_more_btn_elem.click()
+            read_more += 1
+        except NoSuchElementException:
+            pass
+        review_content_elem = review_item_elem.find_element_by_xpath(
+            './div[@class=\'row\']/div/div[@class=\'review-content text-2 margin-vert-small\']')
+        content = review_content_elem.text
 
-    # Rating
-    rating_elem = review_item_elem.find_element_by_class_name('review-rating')
-    full_star_elems = rating_elem.find_elements_by_class_name('icon-star')
-    half_star_elems = rating_elem.find_elements_by_class_name('icon-star-half')
-    if half_star_elems.__len__() > 0:
-        rating = full_star_elems.__len__() + 0.5
-    else:
-        rating = full_star_elems.__len__()
+        # Rating
+        rating_elem = review_item_elem.find_element_by_class_name('review-rating')
+        full_star_elems = rating_elem.find_elements_by_class_name('icon-star')
+        half_star_elems = rating_elem.find_elements_by_class_name('icon-star-half')
+        if half_star_elems.__len__() > 0:
+            rating = full_star_elems.__len__() + 0.5
+        else:
+            rating = full_star_elems.__len__()
 
-    review['content'] = content
-    review['user'] = ''
-    review['rating'] = rating
+        review['content'] = content
+        review['user'] = ''
+        review['rating'] = rating
 
-    reviews.append(review)
+        reviews.append(review)
 
-# Pack Information into a single object
-thread = {'course_id': course['_id'], 'reviews': reviews}
-# thread['course_url'] = course['_id']
+    # Pack Information into a single object
+    thread = {'course_id': course['_id'], 'reviews': reviews}
+    # thread['course_url'] = course['_id']
 
-db_operation_status = CourseraThreads.upsert_courses([thread])
-if not db_operation_status:
-    print('Thread information could not be saved to the database')
+    db_operation_status = CourseraThreads.upsert_courses([thread])
+    if not db_operation_status:
+        print('Thread information could not be saved to the database')
 
-print('Read_more Count:', read_more)
-print('Review Length:', reviews.__len__())
-driver.quit()
-# pprint(course)
+    print('Read_more Count:', read_more)
+    print('Review Length:', reviews.__len__())
+    driver.quit()
+
+    end_time = time.time()
+    time_elapsed = end_time - start_time
+    print('Time Elapsed:', time_elapsed)
+    # pprint(course)
